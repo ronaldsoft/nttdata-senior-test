3b6d9bb9279d23ab474c330ba969fdbd63f42615
Ronald Rivera <ronald.rivera@nextisolutions.com>
2024-10-14 06:36:46 -0500

    cambios cola rabbit

--- cuenta-service/src/main/java/test/cuenta/service/Infrastructure/messaging/RabbitMQConfig.java
+++ cuenta-service/src/main/java/test/cuenta/service/Infrastructure/messaging/RabbitMQConfig.java
@@ -1,9 +1,6 @@
 package test.cuenta.service.Infrastructure.messaging;
 
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,21 +13,16 @@ public class RabbitMQConfig {
     public static final String ROUTING_KEY_CUENTA = "cuenta";
 
     @Bean
-    public Queue clientQueue() {
+    public Queue cuentaQueue() {
         return new Queue(CUENTA_QUEUE);
     }
 
-    // Configurar Jackson como el convertidor de mensajes
-    @Bean
-    public Jackson2JsonMessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
-
-    // Configurar RabbitTemplate para usar Jackson como el convertidor
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(messageConverter());
-        return rabbitTemplate;
-    }
+    // @Bean
+    // public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+    // RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+    // rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter(new
+    // ObjectMapper())); // Configura Jackson
+    // // para enviar JSON
+    // return rabbitTemplate;
+    // }
 }
--- cuenta-service/src/main/java/test/cuenta/service/Interface/ReporteController.java
+++ cuenta-service/src/main/java/test/cuenta/service/Interface/ReporteController.java
@@ -1,13 +1,11 @@
 package test.cuenta.service.Interface;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,43 +31,46 @@ public class ReporteController {
     }
 
     @GetMapping
-    public ResponseEntity<Response<Object>> generarReporte(@RequestParam String fecha, @RequestParam String clienteId)
-            throws ParseException {
-        // Separar la cadena por la coma
-        String[] fechasSeparadas = fecha.split(",");
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        // Obtener las fechas
-        Date fechaInicio = dateFormat.parse(fechasSeparadas[0]);
-        Date fechaFin = dateFormat.parse(fechasSeparadas[1]);
-        long id = Long.parseLong(clienteId);
-        // Solicitar información del cliente
-        // Object cliente = messageProducer.recibirMensaje("client-queue", "1");
-        // if (cliente != null) {
-        // }
-        // System.err.println(cliente.getId());
-        // System.err.println(cliente.getNombre());
-        // if (cliente == null) {
-        // return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente no
-        // encontrado");
-        // }
-        List<Movimiento> movimientos = movimientoRepository.findByCuenta_ClienteidAndFechaBetween(id, fechaInicio,
-                fechaFin);
+    public Response<Object> generarReporte(@RequestParam String fecha, @RequestParam String clienteId) {
+        try {
+            // Separar la cadena por la coma
+            String[] fechasSeparadas = fecha.split(",");
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            // Obtener las fechas
+            Date fechaInicio = dateFormat.parse(fechasSeparadas[0]);
+            Date fechaFin = dateFormat.parse(fechasSeparadas[1]);
+            long id = Long.parseLong(clienteId);
+            // Solicitar información del cliente
+            Object cliente = messageProducer.recibirMensaje("client-queue", clienteId);
+            System.err.println(cliente);
+            if (cliente != null) {
+            }
+            // System.err.println(cliente.getId());
+            // System.err.println(cliente.getNombre());
+            // if (cliente == null) {
+            // return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente no
+            // encontrado");
+            // }
+            List<Movimiento> movimientos = movimientoRepository.findByCuenta_ClienteidAndFechaBetween(id, fechaInicio,
+                    fechaFin);
 
-        // Crear el reporte con la información del cliente y los movimientos
-        List<Reporte> reportes = movimientos.stream().map(movimiento -> {
-            Cuenta cuenta = movimiento.getCuenta(); // Suponiendo que Movimiento tiene una referencia a Cuenta
-            return new Reporte(
-                    movimiento.getFecha(),
-                    "",
-                    cuenta.getNumeroCuenta(),
-                    cuenta.getTipoCuenta(),
-                    cuenta.getSaldoInicial(),
-                    cuenta.getEstado(),
-                    movimiento.getValor(),
-                    (cuenta.getSaldoInicial() + movimiento.getValor()));
-        }).collect(Collectors.toList());
-
-        Response<Object> response = new Response<>(1, "Consulta exitosa", Optional.of(reportes));
-        return ResponseEntity.ok(response);
+            // Crear el reporte con la información del cliente y los movimientos
+            List<Reporte> reportes = movimientos.stream().map(movimiento -> {
+                Cuenta cuenta = movimiento.getCuenta(); // Suponiendo que Movimiento tiene una referencia a Cuenta
+                return new Reporte(
+                        movimiento.getFecha(),
+                        "",
+                        cuenta.getNumeroCuenta(),
+                        cuenta.getTipoCuenta(),
+                        cuenta.getSaldoInicial(),
+                        cuenta.getEstado(),
+                        movimiento.getValor(),
+                        (cuenta.getSaldoInicial() + movimiento.getValor()));
+            }).collect(Collectors.toList());
+            return new Response<>(1, "Lista de movimientos", Optional.of(reportes));
+        } catch (Exception e) {
+            // System.err.println(e.getMessage());
+            return new Response<>(0, e.getMessage(), null); // Devolvemos un error si no se pudo guardar
+        }
     }
 }
